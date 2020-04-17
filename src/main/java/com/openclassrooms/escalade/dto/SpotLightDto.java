e3ba0984ad4d6d163d6a0eb3eb70463b6066125d
Bgt <jordanbouget@gmail.com>
2020-04-17 16:11:00 +0200

    Ajout des photos dans SpotLightDto

--- src/main/java/com/openclassrooms/escalade/dto/SpotLightDto.java
+++ src/main/java/com/openclassrooms/escalade/dto/SpotLightDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,4 +19,5 @@ public class SpotLightDto {
     private Long userId;
     private CotationDto cotationMin;
     private CotationDto cotationMax;
+    private List<PhotoDto> photos;
 }
