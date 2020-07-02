910cd060d0c6b42193b4316e2f15b7562afb5b18
Jordan <jordanbouget@gmail.com>
2020-07-02 12:29:57 +0200

    Modification logique d'update d'un TopoUser
    Pour initialiser une réservation et modification de cette réservation

--- src/main/java/com/openclassrooms/escalade/services/impl/TopoUserServiceImpl.java
+++ src/main/java/com/openclassrooms/escalade/services/impl/TopoUserServiceImpl.java
@@ -4,8 +4,8 @@ import com.openclassrooms.escalade.dao.TopoUserRepository;
 import com.openclassrooms.escalade.dao.UserRepository;
 import com.openclassrooms.escalade.dto.TopoUserDto;
 import com.openclassrooms.escalade.entities.TopoUser;
-import com.openclassrooms.escalade.entities.User;
 import com.openclassrooms.escalade.mapper.TopoUserMapper;
+import com.openclassrooms.escalade.model.EBookingState;
 import com.openclassrooms.escalade.services.TopoUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,14 +23,36 @@ public class TopoUserServiceImpl implements TopoUserService {
     private final UserRepository userRepository;
     private final TopoUserMapper topoUserMapper;
 
-    public TopoUserDto updateTopoUser(TopoUserDto topoUserDto) {
+    public TopoUserDto updateTopoUser(TopoUserDto topoUserDto) { //TODO : réfléchir à un autre algo
+
         TopoUser topoUser = topoUserRepository.findById(topoUserDto.getId()).orElseThrow(EntityNotFoundException::new);
-        User tenant = userRepository.findById(topoUserDto.getTenant().getId())
-                .orElseThrow(EntityNotFoundException::new);
-        topoUser.setAvailable(topoUserDto.isAvailable());
-        topoUser.setTenant(tenant);
-        topoUser.setBookingState(topoUserDto.getBookingState());
-        topoUser.setBookingDate(LocalDateTime.now());
+
+        // Si on a une réservation de faite mais que le statut n'a pas été mis à jour -> problème énum TS
+        if (topoUserDto.getBookingState() == null && topoUserDto.getTenant() != null) {
+            topoUserDto.setBookingState(EBookingState.PENDING);
+        }
+
+        if (topoUserDto.getBookingState() == null) {
+            topoUser.setAvailable(topoUserDto.isAvailable());
+        } else {
+            switch (topoUserDto.getBookingState()) {
+                case ACCEPTED:
+                case PENDING:
+                    topoUser.setTenant(userRepository.findById(topoUserDto.getTenant().getId())
+                            .orElseThrow(EntityNotFoundException::new));
+                    topoUser.setAvailable(false);
+                    topoUser.setBookingState(topoUserDto.getBookingState());
+                    break;
+                case REFUSED:
+                case CANCELED:
+                case FINISHED:
+                    topoUser.setTenant(null);
+                    topoUser.setAvailable(true);
+                    topoUser.setBookingState(null);
+                    break;
+            }
+            topoUser.setBookingDate(LocalDateTime.now());
+        }
         return topoUserMapper.toTopoUserDto(topoUserRepository.save(topoUser));
     }
 }
